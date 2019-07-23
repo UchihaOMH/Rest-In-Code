a5cf90fef1723c4136dc7361d63836e698736900
오명현 <pppolk@naver.com>
2019-07-23 13:21:09 +0900

    카메라 워크 구현

---
+++ Train To Busan/Assets/Script/Camera_Follow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform camTr;
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        camTr.position = new Vector3(target.position.x, camTr.position.y, camTr.position.z);
+    }
+}
--- Train To Busan/Assets/Script/Controller.cs
+++ Train To Busan/Assets/Script/Controller.cs
@@ -18,7 +18,22 @@ public class Controller : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey)
+        if (Input.GetKey(KeyCode.W))
+        {
+            doll.Translate(Vector3.up * runSpeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            doll.Translate(Vector3.up * -runSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            doll.Translate(Vector3.right * runSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            doll.Translate(Vector3.right * -runSpeed);
+        }
     }
 #endif
 
