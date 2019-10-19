609410cdb787edf4327abadd5f3bb565f46881fd
오명현 <pppolk@naver.com>
2019-10-19 13:31:27 +0900

    2019.10.19 오후 1:31
    새 프로젝트 로딩

---
+++ Travel To Busan/Assets/2.Scripts/Prologue Scripts/GameConst.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameConst
+{
+    public enum SpriteDefaultDirection
+    {
+        Left,
+        Right
+    }
+
+    public static class AnimationParam
+    {
+        public enum eAnim
+        {
+            Run,
+            Idle,
+            Die,
+            Jump,
+            Sit,
+            Roll,
+            Punch,
+            Kick
+        }
+
+        #region Animator Parameters
+        public const string tRun = "tRun";
+        public const string tIdle = "tIdle";
+        public const string tDie = "tDie";
+        public const string tJump = "tJump";
+        public const string tSit = "tSit";
+        public const string tRoll = "tRoll";
+        public const string tKick = "tKick";
+        public const string tPunch = "tPunch";
+        public const string tResurrect = "tResurrect";
+
+        public const string iComboCount = "iComboCount";
+        #endregion
+    }
+}
